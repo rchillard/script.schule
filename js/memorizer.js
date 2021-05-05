3d67e42af02544aca5f3ecbb69a5402919ca5b55
Ryan Hillard <rchillard@gmail.com>
2021-05-05 00:04:20 -0500

    WIP of loading questions and next test date from local storage

--- js/memorizer.js
+++ js/memorizer.js
@@ -1,40 +1,62 @@
 // memorizer.js - logic to support a very primitive spaced repetition algorithm
 //
 // Difficulty   Repetition
-// Easy         6 days
+// Easy         7 days
 // Medium       3 days
 // Hard         1 day
 
-// Question and Answer Box
+// Elements on page
 var question = document.querySelector('#questionBox');
 var answer = document.querySelector('#answerBox');
+var card = document.querySelector('.card');
+var difficulty = document.querySelector('.difficulty');
 
 // Retrieve list of all elements
-var elements = {};
+var elements = {
+    "abbr": "",
+    "div": "",
+    "label": ""
+};
 
-fetch('https://html.haus/api/index.json').then(function (response) {
-    // The API call was successful, so check if response is valid (200)
-    if (response.ok) {
-        return response.json();
-    } else {
-        return Promise.reject(response);
-    }
-}).then(function (data) {
-    // data is the JSON response
-    elements = data.json();
-}).catch(function (err) {
-    // err is the raw response
-    console.warn(`Error fetching question data: ${err}`);
-})
+// fetch('https://html.haus/api/index.json').then(function (response) {
+//     // The API call was successful, so check if response is valid (200)
+//     if (response.ok) {
+//         return response.json();
+//     } else {
+//         return Promise.reject(response);
+//     }
+// }).then(function (data) {
+//     // data is the JSON response
+//     elements = data;
+//     console.log(elements);
+// }).catch(function (err) {
+//     // err is the raw response
+//     console.warn(`Error fetching question data: ${err}`);
+// })
 
 // Load local storage version
+var keys = Object.keys(localStorage);
+console.log(keys);
+for (var key of keys) {
+    // If this element exists in the elements object
+    console.log(key);
+    if (elements.hasOwnProperty(key)) {
+        console.log(elements.key);
+        // Assign the next date as the value for this key
+        elements[key] = localStorage.getItem(key);
+    }
+    console.log(`${key}: ${localStorage.getItem(key)}`);
+    console.log(elements.key);
+}
 
 // Put in proper practice order for questionDeck
+// var sortedQuestions = elements.slice().sort((a,b) => b.date - a.date);
 
+console.log(sortedQuestions);
 // Load the first question
 var currentQuestion = {};
 
-fetch('https://html.haus/api/elements/abbr.json').then(function (response) {
+fetch('https://html.haus/api/elements/label.json').then(function (response) {
     // The API call was successful, so check if response is valid (200)
     if (response.ok) {
         return response.json();
@@ -43,7 +65,6 @@ fetch('https://html.haus/api/elements/abbr.json').then(function (response) {
     }
 }).then(function (data) {
     // data is the JSON response
-    console.log(data);
     currentQuestion = data;
     question.textContent = currentQuestion.name;
     answer.textContent = currentQuestion.description;
@@ -52,35 +73,36 @@ fetch('https://html.haus/api/elements/abbr.json').then(function (response) {
     console.warn(`Error fetching question data: ${err}`);
 })
 
-// Elements on the actual page
-var card = document.querySelector('.card');
-var difficulty = document.querySelector('.difficulty');
-
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
 });
 
 difficulty.addEventListener('click', function (event) {
+    var nextTestDate = new Date();
+    
     if (event.target.id === "easy") {
         console.log("Easy!");
         // Assign next time this question comes up
-        recordedDifficulty = "easy";
+        nextTestDate.setDate(nextTestDate.getDate() + 7)
+        recordedDifficulty = nextTestDate;
     }
 
     if (event.target.id === "medium") {
         console.log("Medium!");
         // Assign next time this question comes up
-        recordedDifficulty = "medium";
+        nextTestDate.setDate(nextTestDate.getDate() + 3)
+        recordedDifficulty = nextTestDate;
     }
 
     if (event.target.id === "hard") {
         console.log("Hard!");
         // Assign next time this question comes up
-        recordedDifficulty = "hard";
+        nextTestDate.setDate(nextTestDate.getDate() + 1)
+        recordedDifficulty = nextTestDate;
     }
 
     // Save result to localStorage
-    var cardDifficulty = localStorage.setItem(question.textContent, recordedDifficulty);
+    localStorage.setItem(question.textContent, recordedDifficulty);
 })
 
 
