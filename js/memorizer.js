235298349295d559be0e162d87dafda61490331f
Ryan Hillard <rchillard@gmail.com>
2021-05-04 23:30:52 -0500

    WIP of loading JSON data for questions into DOM

--- js/memorizer.js
+++ js/memorizer.js
@@ -1,5 +1,58 @@
-// memorizer.js
+// memorizer.js - logic to support a very primitive spaced repetition algorithm
+//
+// Difficulty   Repetition
+// Easy         6 days
+// Medium       3 days
+// Hard         1 day
 
+// Question and Answer Box
+var question = document.querySelector('#questionBox');
+var answer = document.querySelector('#answerBox');
+
+// Retrieve list of all elements
+var elements = {};
+
+fetch('https://html.haus/api/index.json').then(function (response) {
+    // The API call was successful, so check if response is valid (200)
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
+    }
+}).then(function (data) {
+    // data is the JSON response
+    elements = data.json();
+}).catch(function (err) {
+    // err is the raw response
+    console.warn(`Error fetching question data: ${err}`);
+})
+
+// Load local storage version
+
+// Put in proper practice order for questionDeck
+
+// Load the first question
+var currentQuestion = {};
+
+fetch('https://html.haus/api/elements/abbr.json').then(function (response) {
+    // The API call was successful, so check if response is valid (200)
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
+    }
+}).then(function (data) {
+    // data is the JSON response
+    console.log(data);
+    currentQuestion = data;
+    question.textContent = currentQuestion.name;
+    answer.textContent = currentQuestion.description;
+}).catch(function (err) {
+    // err is the raw response
+    console.warn(`Error fetching question data: ${err}`);
+})
+
+// Elements on the actual page
 var card = document.querySelector('.card');
 var difficulty = document.querySelector('.difficulty');
 
@@ -8,47 +61,27 @@ card.addEventListener('click', function () {
 });
 
 difficulty.addEventListener('click', function (event) {
-    if(event.target.id==="easy") {
-        console.log("Easy!")
+    if (event.target.id === "easy") {
+        console.log("Easy!");
         // Assign next time this question comes up
         recordedDifficulty = "easy";
     }
 
-    if(event.target.id==="medium") {
-        console.log("Medium!")
+    if (event.target.id === "medium") {
+        console.log("Medium!");
         // Assign next time this question comes up
         recordedDifficulty = "medium";
     }
 
-    if(event.target.id==="hard") {
-        console.log("Hard!")
+    if (event.target.id === "hard") {
+        console.log("Hard!");
         // Assign next time this question comes up
         recordedDifficulty = "hard";
     }
 
     // Save result to localStorage
-    var cardDifficulty = localStorage.setItem(questionElement.textContent, recordedDifficulty)
+    var cardDifficulty = localStorage.setItem(question.textContent, recordedDifficulty);
 })
 
-// Spaced repetition table
-// Difficulty   Repetition
-// Easy         6 days
-// Medium       3 days
-// Hard         1 day
 
-// fetch('https://script.schule/data/dom-manipulation.json').then(function (response) {
-//     // The API call was successful, so check if response is valid (200)
-//     if (response.ok) {
-//         return response.json();
-//     } else {
-//         return Promise.reject(response);
-//     }
-// }).then(function (data) {
-//     // data is the JSON response
-//     questions = data;
-//     questionElement.textContent = questions[currentQuestion].question;
-//     answerElement.textContent = questions[currentQuestion].answer;
-// }).catch(function (err) {
-//     // err is the raw response
-//     questionElement.value = `Error fetching question data: ${err}`;
-// })
+
