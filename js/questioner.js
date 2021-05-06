cbdf964fc3e0e551cb3ef1f64bab472d2a60dec1
RCH <rchillard@gmail.com>
2021-05-06 00:17:08 -0500

    Add card flipper (#1)
    * Basic card flipper created as work in progress
    
    * Add hover styling for easy, medium, and hard difficulty radio buttons
    
    * Add WIP of card flipper
    
    * Breakout memorizer as separate file from questioner
    
    * WIP of loading JSON data for questions into DOM
    
    * WIP of loading questions and next test date from local storage
    
    * Update styles for better UX on card hover, remove unnecessary buttons
    
    * Basic WIP working for loading data, going to next question
    
    * Fix event delegation bug and animation oddity

--- js/questioner.js
+++ js/questioner.js
@@ -5,18 +5,19 @@ var resultElement = document.getElementById('result');
 var questions;
 var currentQuestion = 0;
 
-fetch('https://script.schule/data/dom-manipulation.json').then(function(response) {
+fetch('https://script.schule/data/dom-manipulation.json').then(function (response) {
     // The API call was successful, so check if response is valid (200)
-    if(response.ok) {
+    if (response.ok) {
         return response.json();
     } else {
         return Promise.reject(response);
     }
-}).then(function(data) {
+}).then(function (data) {
     // data is the JSON response
     questions = data;
     questionElement.textContent = questions[currentQuestion].question;
-}).catch(function(err) {
+    answerElement.textContent = questions[currentQuestion].answer;
+}).catch(function (err) {
     // err is the raw response
     questionElement.value = `Error fetching question data: ${err}`;
 })
@@ -26,7 +27,7 @@ function checkAnswer() {
         console.log(`Question ${currentQuestion}: Correct`);
         resultElement.textContent = "Correct";
         resultElement.classList.remove('hidden');
-        window.setTimeout(function() {
+        window.setTimeout(function () {
             currentQuestion = currentQuestion + 1;
             resultElement.classList.add('hidden');
             questionElement.textContent = questions[currentQuestion].question;
@@ -36,7 +37,7 @@ function checkAnswer() {
         console.log("Try Again");
         resultElement.textContent = "Try Again";
         resultElement.classList.remove('hidden');
-        window.setTimeout(function() { resultElement.classList.add('hidden') }, 500);
+        window.setTimeout(function () { resultElement.classList.add('hidden') }, 500);
     }
 }
 
